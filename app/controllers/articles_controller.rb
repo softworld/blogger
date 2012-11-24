c168b2f8b4420fecbd7eb29c69b1473269a9eb6f
Adrian Nutiu <adrian@nutiu.com>
2012-11-24 21:43:08 +0200

    Initial commit after adding article and comment models

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(params[:article])
+		@article.save
+
+    flash.notice = "Successfully created '#{@article.title}'"
+		redirect_to article_path(@article) 
+	end
+
+	def destroy
+		@article = Article.destroy(params[:id])
+		flash.notice= "Successfully deleted '#{@article.title}'"
+		redirect_to articles_path
+	end
+
+  def edit
+  	@article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+    
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'beb0cb59340299095ec7d703f4e4e10e8261a64c050d3f159a258735a7cff912f52be32976d45845854922d797ac789eb7a3499fd81634c25affc3cc269ae4e9'
